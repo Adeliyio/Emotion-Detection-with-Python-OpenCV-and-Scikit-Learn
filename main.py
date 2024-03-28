5da757abfbe504ac877193da781425e4787ad110
Adeliyio <adeliyitomiwa@yahoo.com>
2024-03-28 13:15:13 +0000

    Utils and Prepare_Data.py Added

---
+++ main.py
@@ -0,0 +1,13 @@
+from utils import get_face_landmarks
+import os
+import cv2
+
+data_dir = './Data1'
+
+if __name__ == "__main__":
+    for emotion_indx, emotion in enumerate(sorted(os.listdir(data_dir))):
+        for image_path_ in os.listdir(os.path.join(data_dir, emotion)):
+            image_path = os.path.join(data_dir, emotion, image_path_)
+            image = cv2.imread(image_path)
+            face_landmarks = get_face_landmarks(image)
+            print(len(face_landmarks))
